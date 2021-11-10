4cb8b3a93a0eecef5b8d76ee84d2f5816b07bbc5
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-11-10 13:42:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   int n=0;
+   cin>>n;
+   if (n==0){
+       cout<<"errore, scegli un numero maggiore di 0"<<endl;
+   }
+   else{
+       cout<<"1"<<endl;
+       int a=0, b=1, c=0;
+
+       for (int i = 0; i < n; i++){
+           c=a+b;
+           a=b;
+           b=c;
+           if(c<=n){
+               cout << c << endl;
+           }else{break;}
+       }
+   }
    return 0;
 }
